f64d353b8190876b95aab90857e78ee66d2d7626
kenta-s <knt01222@gmail.com>
2017-07-09 22:23:38 +0900

    Write a model as a Chain

---
+++ introduction/write_a_model_as_a_chain.py
@@ -0,0 +1,21 @@
+import numpy as np
+import chainer
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+
+from IPython import embed
+
+class MyChain(Chain):
+    def __init__(self):
+        super(MyChain, self).__init__()
+        with self.init_scope():
+            self.l1 = L.Linear(4, 3)
+            self.l2 = L.Linear(3, 2)
+
+    def __call__(self, x):
+        h = self.l1(x)
+        return self.l2(h)
